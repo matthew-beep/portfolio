17216841f85d4d49eb8913311a8f104bab530cc9
matthew-beep <herram@uw.edu>
2023-06-20 12:01:28 -0700

    Added tailwind

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,4 +1,5 @@
 import { React } from 'react';
+import './styles.css';
 export default function About() {
     return (      
         <div className='container home'>
--- portfolio/src/components/Nav.js
+++ portfolio/src/components/Nav.js
@@ -10,7 +10,7 @@ export default function Nav(props) {
 
     
     return (
-        <nav className='container col-lg-4' id = "myDIV">
+        <nav className='container col-lg-4 w-1/4' id = "myDIV">
             <ul>
                 <li id="home-link" >Home</li>
                 <li >About</li>
