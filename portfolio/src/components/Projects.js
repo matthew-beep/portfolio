5052226a2f7ede0a70b6bf3d86c28a8cf737c09f
matthew-beep <herram@uw.edu>
2023-06-23 21:34:08 -0700

    95% done with info hover

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,4 +1,5 @@
 import { React } from 'react';
+import imdbDoc from './files/imdbDoc.pdf';
 import imdbLogo from './img/imdb_svg.svg';
 
 
@@ -20,7 +21,8 @@ export default function Projects(props) {
                                 <h4>UX / UI Design | Full Stack Development</h4>
                                 <p>The first ever public facing Distributed Acoustic Sensing research tool. Built in collaboration with 
                                     UW's Earth and Space Science Department and recognized as a Research Award Finalist at the iSchool 
-                                    Capstone Gala.</p>
+                                    Capstone Gala.
+                                </p>
                                 <p><strong>Skills:</strong> HTML/CSS, JavaScript, Python, Leaflet, MUI</p>
                             </div>
                             <div className='links flex w-full bottom-0 absolute'>
@@ -37,24 +39,27 @@ export default function Projects(props) {
                     </div>
                     <div className='mb-5 w-96 bg-[#F6C700] rounded-md border-[#CBCBCB] border-1 border-solid h-80 flex justify-center align-center project-container'>
                         <img src={imdbLogo} alt='IMDb Logo'/>
-                        <div className='info px-2'>
-                            <div className='flex flex-col border-2 border-solid justify-around text-left h-full relative'>
-                                <div className='h-full flex flex-col justify-between border-2 border-solid'>
-                                    <h3 className='black text-3xl underline decoration-purple'>IMDb Redesign</h3>
-                                    <div>
-                                        <h4 className='bold text-lg'>UX Design | Information Architecture</h4>
-                                        <p>An analysis of the Internet Movie Database's information architecture. 
-                                            Includes a collection of recommendations for improvement on IMDb's hierarchy of information related to 
-                                            labels, search, navigation, site mapping, etc.
-                                        </p>
-                                        <p><strong>Skills:</strong> User Testing, Wireframing</p>
+                        <div className='info py-2 px-3'>
+                            <div className='flex flex-col justify-around text-left h-full relative'>
+                                <div className='h-full'>
+                                    <h3 className='bold text-3xl mb-2'>IMDb Redesign</h3>  
+                                    <div className='flex mb-2'>
+                                        <div className='pill bold mr-2'>
+                                            <h4>UX Design</h4>
+                                        </div>
+                                        <div className='pill bold'>
+                                            <h4>Information Architecture</h4>
+                                        </div>
                                     </div>
+                                    <p>An analysis of the Internet Movie Database's information architecture. 
+                                        Includes a collection of recommendations for improvement on IMDb's hierarchy of information related to 
+                                        labels, search, navigation, site mapping, etc.
+                                    </p>
+                                    <p className='mt-2'><strong>Skills:</strong> User Testing, Personas, Wireframing</p>
                                 </div>
-                                <div className='links flex w-full'>
-                                    <div className='m-auto mb-2 flex items-center'>
-                                        <a href='#'>Full Case Study</a> <div className='mx-1 m-auto'> | </div>
-                                        <a href='#'>Presentation</a>
-                                    </div>
+                                <div className='links flex w-full'>     
+                                    <a href={imdbDoc} target='_blank' className='btn-link mr-2 bold text-lg'>Full Case Study</a> 
+                                    <a href='#' className='btn-link bold text-lg'>Presentation</a>
                                 </div>
                             </div>
                         </div>
